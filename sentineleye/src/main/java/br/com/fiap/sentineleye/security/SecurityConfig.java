115e0ecc998781c705392cafc206d5d1a43cfe08
FellipeCostaOliveira <fellipe20070305@gmail.com>
2026-06-05 23:28:14 -0300

    Primeiro Commit

---
+++ sentineleye/src/main/java/br/com/fiap/sentineleye/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap.sentineleye.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+	@Autowired
+	private JWTAuthFilter authFilter;
+
+	@Bean
+	public SecurityFilterChain filtrar(HttpSecurity request) throws Exception {
+		request
+			.cors(cors -> {})
+			.csrf(csrf -> csrf.disable())
+			.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+			.authorizeHttpRequests(req -> req.anyRequest().permitAll())
+			.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		return request.build();
+	}
+}
